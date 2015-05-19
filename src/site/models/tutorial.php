f7a438979e6d691ac21f5d89eae8575631711511
Bill Tomczak <bill@ostraining.com>
2015-05-19 16:37:17 -0700

    Cleanup site model code

--- src/site/models/tutorial.php
+++ src/site/models/tutorial.php
@@ -3,35 +3,41 @@ defined('_JEXEC') or die;
 
 class OSToolbarModelTutorial extends OstoolbarModel
 {
+    /**
+     * @var object
+     */
+    protected $data = null;
+
     protected function populateState()
     {
-        $id = JRequest::getVar('cid', array(), 'request', 'array');
-        if (empty($id)) :
-            $id = JRequest::getInt('id', 0);
-        else :
-            $id = $id[0];
-        endif;
+        $app = JFactory::getApplication();
 
+        $id = $app->get('cid', array(), 'request', 'array');
+        if (empty($id)) {
+            $id = $app->input->getInt('id', 0);
+        } else {
+            $id = $id[0];
+        }
         $this->setState('id', $id);
     }
 
     public function getData()
     {
         $id         = $this->getState('id', 0);
-        $this->data = OstoolbarCache::callback($this, '_fetchData', array($id));
+        $this->data = OstoolbarCache::callback($this, 'fetchData', array($id));
+
         return $this->data;
     }
 
-    public function _fetchData($id)
+    public function fetchData($id)
     {
         $data = array('resource' => 'article', 'id' => $id);
 
         $response = OstoolbarRequest::makeRequest($data);
-        if ($response->hasError()) :
+        if ($response->hasError()) {
             JFactory::getApplication()->enqueueMessage(JText::_('COM_OSTOOLBAR_API_KEY_ERROR'), 'error');
-            //$this->setError('Error:  '.$response->getErrorMsg().' (Code '.$response->getErrorCode().')');
             return false;
-        endif;
+        }
 
         $body            = $response->getBody();
         $body->introtext = OstoolbarRequest::filter($body->introtext);
--- src/site/models/tutorials.php
+++ src/site/models/tutorials.php
@@ -4,20 +4,25 @@ jimport('joomla.application.component.modellegacy');
 
 class OSToolbarModelTutorials extends OstoolbarModel
 {
+    /**
+     * @var array
+     */
+    protected $data = array();
+
     public function getList()
     {
-        $this->data = OstoolbarCache::callback($this, '_fetchList', array(), null, true);
+        $this->data = OstoolbarCache::callback($this, 'fetchList', array(), null, true);
 
         $params   = JComponentHelper::getParams('com_ostoolbar');
         $selected = array();
-        if ($params->get("videos")) {
-            $selected = preg_split("/,/", $params->get("videos"));
+        if ($params->get('videos')) {
+            $selected = preg_split('/,/', $params->get('videos'));
         }
         if (count($selected)) {
             $data = array();
             foreach ($selected as $item) {
                 foreach ($this->data as $row) {
-                    if ("s_" . $row->id == $item) {
+                    if ('s_' . $row->id == $item) {
                         $data[] = $row;
                         break;
                     }
@@ -29,22 +34,20 @@ class OSToolbarModelTutorials extends OstoolbarModel
         return $this->data;
     }
 
-    public function _fetchList()
+    public function fetchList()
     {
-        $data = array('resource' => 'articles');
-
+        $data     = array('resource' => 'articles');
         $response = OstoolbarRequest::makeRequest($data);
 
-        if ($response->hasError()) :
+        if ($response->hasError()) {
             JFactory::getApplication()->enqueueMessage(JText::_('COM_OSTOOLBAR_API_KEY_ERROR'), 'error');
             return false;
-        endif;
+        }
 
         $list = $response->getBody();
-
-        for ($i = 0; $i < count($list); $i++) :
+        for ($i = 0; $i < count($list); $i++) {
             $list[$i]->link = 'index.php?option=com_ostoolbar&view=tutorial&id=' . $list[$i]->id;
-        endfor;
+        }
 
         return $list;
     }
