a6c24c5a92a45276258ec223f5ad1462c94256ea
Bill Tomczak <bill@ostraining.com>
2015-06-09 15:39:38 -0700

    Cleanup version fetching, Fix some code style issues

--- src/admin/models/tutorial.php
+++ src/admin/models/tutorial.php
@@ -29,24 +29,28 @@ class OstoolbarModelTutorial extends OstoolbarModel
     public function getData()
     {
         $id         = $this->getState('id', 0);
-        $this->data = OstoolbarCache::callback($this, '_fetchData', array($id));
+        $this->data = OstoolbarCache::callback($this, 'fetchData', array($id));
         return $this->data;
     }
 
-    public function _fetchData($id)
+    public function fetchData($id)
     {
         $data = array('resource' => 'article', 'id' => $id);
 
         $response = OstoolbarRequest::makeRequest($data);
-        if ($response->hasError()) :
+        if ($response->hasError()) {
             JFactory::getApplication()->enqueueMessage(JText::_('COM_OSTOOLBAR_API_KEY_ERROR'), 'error');
             return false;
-        endif;
+        }
 
-        $body            = $response->getBody();
-        $body->introtext = OstoolbarRequest::filter($body->introtext);
-        $body->fulltext  = OstoolbarRequest::filter($body->fulltext);
+        $body = $response->getBody();
 
-        return $body;
+        // @TODO: A terrible way to get what we want
+        list(, $urlVars) = explode('?', OstoolbarRequest::getHostUrl());
+        parse_str($urlVars, $urlQuery);
+        $body->jversion = $urlQuery['v'];
+        // End terribleness
+
+        return $response->getBody();
     }
 }
