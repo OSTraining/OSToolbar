296e447637024fa598c31c5f64b931ad4388a18e
Bill Tomczak <bill@ostraining.com>
2016-03-29 16:33:12 -0700

    Add more graceful message when content not found

--- src/admin/views/tutorial/tmpl/default.php
+++ src/admin/views/tutorial/tmpl/default.php
@@ -11,21 +11,31 @@ defined('_JEXEC') or die();
 ?>
 <div class="ostoolbar">
     <fieldset class='adminform'>
-        <legend><?php echo $this->row->title; ?></legend>
-        <form action="index.php" method="post" name='adminForm'>
-            <table width='100%' cellpadding='5' cellspacing='0' class='admintable form-validate'>
-                <tr>
-                    <td><?php echo $this->row->introtext . $this->row->fulltext; ?></td>
-                </tr>
-            </table>
+        <?php
+        if ($this->row) :
+            ?>
+            <legend><?php echo $this->row->title; ?></legend>
+            <form action="index.php" method="post" name='adminForm'>
+                <table width='100%' cellpadding='5' cellspacing='0' class='admintable form-validate'>
+                    <tr>
+                        <td><?php echo $this->row->introtext . $this->row->fulltext; ?></td>
+                    </tr>
+                </table>
 
-            <input type='hidden' name='id' id='id' value='<?php echo $this->row->id; ?>'/>
-            <input type="hidden" name="task" id="task" value=""/>
-            <input type="hidden" name="c" id="c" value="tutorial"/>
-            <input type="hidden" name="ret" id="ret" value="<?php echo $this->return; ?>"/>
-            <input type="hidden" name="option" id="option" value="<?php echo $this->option; ?>"/>
-            <?php echo JHTML::_('form.token'); ?>
-        </form>
+                <input type='hidden' name='id' id='id' value='<?php echo $this->row->id; ?>'/>
+                <input type="hidden" name="task" id="task" value=""/>
+                <input type="hidden" name="c" id="c" value="tutorial"/>
+                <input type="hidden" name="ret" id="ret" value="<?php echo $this->return; ?>"/>
+                <input type="hidden" name="option" id="option" value="<?php echo $this->option; ?>"/>
+                <?php echo JHTML::_('form.token'); ?>
+            </form>
+            <?php
+        else :
+            ?>
+            <p><?php echo JText::_('COM_OSTOOLBAR_ERROR_NO_VIDEO'); ?></p>
+            <?php
+        endif;
+        ?>
     </fieldset>
     <?php echo OstoolbarHelper::getTrialBanner(); ?>
 </div>
--- src/admin/views/tutorial/tmpl/popup.php
+++ src/admin/views/tutorial/tmpl/popup.php
@@ -2,7 +2,17 @@
 defined('_JEXEC') or die('Restricted access');
 ?>
 <div class="ostoolbar">
+    <?php
+    if ($this->row) :
+    ?>
     <h1 class="popup_tutorial_title"><?php echo $this->row->title; ?></h1>
 
     <div class="popup_tutorial_text"><?php echo $this->row->introtext . $this->row->fulltext; ?></div>
+    <?php
+    else :
+    ?>
+        <p><?php echo JText::_('COM_OSTOOLBAR_ERROR_NO_VIDEO'); ?></p>
+    <?php
+    endif;
+    ?>
 </div>
